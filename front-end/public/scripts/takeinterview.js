3c196a2fc18b4e85d75ceba2cdc1640d91497e53
James Burnside <mail@jamesburnside.com>
2015-12-21 20:20:11 +0000

    added next page - the select your type of interview page

--- front-end/public/scripts/takeinterview.js
+++ front-end/public/scripts/takeinterview.js
@@ -4,32 +4,115 @@ var InterviewApp = React.createClass({
 	},
 
 	nextStage: function() {
-		console.log("next stage");
 		this.setState({currentStage: this.state.currentStage+1});
 	},
 
 	render: function() {
 		switch(this.state.currentStage){
-			case 0: return (<GetStarted nextStage={this.nextStage}/>); break;
+			case 0: return (<InterviewInstructions nextStage={this.nextStage}/>); break;
 			case 1: return (<InterviewDetails nextStage={this.nextStage}/>); break;
+			case 2: return (<InterviewReview nextStage={this.nextStage}/>); break;
 			default: null;
 		}
 			
 	}
 });
 
+var InterviewReview = React.createClass({
+	render: function() {
+		return (
+			<section className="white bluetop minH">
+				<div className="container">
+				</div>
+			</section>
+		);
+	}
+});
+
 var InterviewDetails = React.createClass({
+
 	render: function() {
 		return (
 			<div>
-				<section className="white minH">
+				<section className="white bluetop">
+					<div className="container">
+						<h1>Choose your interview</h1>
+						<br />
+						<br />
+						<h4>You are logged in as <span className="emph">djprof</span></h4>
+						<table className="mt20">
+							<tbody>
+								<tr className="interview_choice">
+									<td>
+										<h4>Choose interview catagory:</h4> 
+									</td>
+									<td>
+										<select>
+											<option value="strings">Strings</option>
+											<option value="linkedlists">LinkedLists</option>
+											<option value="bitmanipulation">Bit Manipulation</option>
+											<option value="random">Random</option>
+										</select>
+									</td>
+								</tr>
+								<tr className="interview_choice">
+									<td>
+										<h4>Choose interview language:</h4> 
+									</td>
+									<td>
+										<select>
+											<option value="python">python</option>
+											<option value="C++">C++</option>
+											<option value="java">java</option>
+											<option value="javascript">javascript</option>
+											<option value="haskell">haskell</option>
+
+										</select>
+									</td>
+								</tr>
+							</tbody>
+						</table>
+
+						<h4>Choose interview difficulty:</h4> 
+						<div className="option_button ml70 option_button_selected">easy</div>
+						<div className="option_button">medium</div>
+						<div className="option_button">hard</div>
+						<div className="option_button">expert</div>
+						<br />
+						<br />
+
+						<h4>Choose interview length:</h4> 
+						<div className="option_button ml70">15mins</div>
+						<div className="option_button">30mins</div>
+						<div className="option_button option_button_selected">1hour</div>
+						<div className="option_button">4x1hour</div>
+						<br />
+						<br />
+
+						<h4>Choose recording type:</h4> 
+						<div className="option_button ml70 option_button_selected">video, audio, text</div>
+						<div className="option_button">audio, text</div>
+						<div className="option_button">text, only</div>
+						<br />
+						<br />
+
+						<h4>Finally, enter a name for your video:</h4> 
+						<input className="ml70" type="text" name="video_title" placeholder="enter video name here" />
+						<br />
+						<br />
+						<br />
+						<br />
+
+						<div onClick={this.props.nextStage} className="button">Review Settings</div>
+					</div>
+
 				</section>
 			</div>
 		);
 	}
 });
 
-var GetStarted = React.createClass({
+var InterviewInstructions = React.createClass({
 	render: function() {
 		return (
 			<div>

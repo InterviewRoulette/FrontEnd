b9d66c952ede40745871460911dd76f20abf276c
James Burnside <mail@jamesburnside.com>
2015-12-24 21:54:05 +0000

    added stuff to interview.html page

--- front-end/public/scripts/interview.js
+++ front-end/public/scripts/interview.js
@@ -14,6 +14,19 @@ var Interview = React.createClass({
 				<section className="white minH bluetop">
 					<div className="container">
 						<h1>{this.state.interviewdata.title}</h1>
+						<br />
+						<br />
+						<div>
+							<video src="/" className="video_capture_window" controls></video>
+							<textarea className="coding_capture_window" defaultValue="#include <stdlib.c>&#13;&#13;int main(int argc, char **argv)&#13;{&#13;&nbsp;&nbsp;&nbsp;&nbsp;printf('hello world\n');&#13;&#13;&nbsp;&nbsp;&nbsp;&nbsp;return 0;&#13;}"/>
+						</div>
+
+						<div className="comment_container">
+							<h4>{'Comments ('+this.state.interviewdata.comments.length+')'}</h4>
+							<div className="comment_box">
+								
+							</div>
+						</div>
 					</div>
 				</section>
 			</div>
